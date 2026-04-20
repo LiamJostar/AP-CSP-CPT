0b14ed1cc11e53b01152b710a95d26b02474c036
Liam J <wjester883@gmail.com>
2026-04-20 18:24:51 +0000

    .lower( )
    Co-authored-by: sirchance123 <sirchance123@users.noreply.github.com>

--- AP_stuffs.py
+++ AP_stuffs.py
@@ -1,165 +1,173 @@
 import random
 import time
-Loot_type = ["Primary Weapon", "Secondary Weapon", "Currency", "Armor"]
-Loot_pri = ["Sword", "Bow", "Axe", "Sythe"]
-Loot_sec = ["Shield", "Dagger", "Dart", "Chakram"]
-Loot_curr = ["Coins", "Gems", "Dark Dollars", "Tokens"]
-Curr_amt = ["1", "2", "3", "4", "5"]
-Loot_armor = ["Helm", "Chestpiece", "Greaves", "Boots"]
-Rare_Sword = ["Bronze Greatsword", "Bronze Shortsword", "Bronze Longsword", "Bronze Rapier"]
-Rare_Bow = ["Longbow", "Shortbow", "Strongbow", "Quickbow"]
-Rare_Axe = ["Dual Axe", "Great axe", "Handaxe", "Double Handaxe"]
-Rare_Sythe = ["Poison sythe", "Quicksythe", "Dual sythe", "Syckle"]
-SupRare_Sword = ["Iron Greatsword", "Iron Shortsword", "Iron Longsword", "Iron Rapier"]
-SupRare_Bow = ["Flarebow", "Shockbow", "Barbed Bow", "Freezebow"]
-SupRare_Axe = ["Waraxe", "Shockaxe", "Flareaxe", "Freezeaxe"]
-SupRare_Sythe = ["Poison Syckle", "Cursed sythe", "Circus Blade", "Dual Poison Sythe"]
-Mythic_Sword = ["Nightmare Blade", "Roaring Shortsword", "Dragonscale Blade", "God's Wisdom"]
-Mythic_Bow = ["Nightmare Bow", "God's Sight", "Fay Bow", "Dwarven Craft"]
-Mythic_Axe = ["Nightmare Axe", "God's Rage", "Rude Buster", "Dwarven Hammer"]
-Mythic_Sythe = ["Nightmare Sythe", "God's Plague", "Jevilsknife", "Reaper"]
-Rare_helm = ["Leather Helm", "Bronze Helm", "Quick Helm", "Strong Helm"]
-SupRare_helm = ["Iron Helm", "Flame Helm", "Spider Helm", "Shock Helm"]
-Mythic_helm = ["Nightmare Helm", "Hellfire Helm", "God's Touch", "Jevilstail"]
-Rare_Chestpiece = ["Leather Chestpiece", "Bronze Chestpiece", "Plate Mail", "Aluminum Chestpiece"]
-SupRare_Chestpiece = ["Iron Chestpiece", "Silver Chestpiece", "Rusted Knight Chestpiece", "Tin Chestpiece"]
-Mythic_Chestpiece = ["Fallen Knight's Chestpiece", "Nightmare Chestpiece", "God's Heart", "The Hero's Chestpiece"]
-Rare_Greaves = ["Wooden Greaves", "Leather Greaves", "Leaf Greaves", "Clay Greaves"]
-SupRare_Greaves = ["Iron Greaves", "Silver Greaves", "Rusted Knight Greaves", "Tin Greaves"]
-Mythic_Greaves = ["Fallen Knight's Greaves", "Nightmare Greaves", "God's Speed", "The Monarch's Greaves"]
-Rare_Boots = ["Leather Boots", "Wooden Boots", "Foil Boots", "Light Weight Boots"]
-SupRare_Boots = ["Hermes Boots", "Steel Boots", "Iron Boots", "Heavy Weight Boots"]
-Mythic_Boots = ["TerraSpark Boots", "Fallen Knight's Boots", "Nightmare Boots", "The Mourner's Boots", "God's Remorse"]
-Rare_Shield = ["Wooden Shield", "Aluminum Shield", "Rusted Metal Shield", "Cardboard Shield"]
-SupRare_Shield = ["Iron Shield", "Silver Shield", "Freezing Shield", "Flame Shield"]
-Mythic_Shield = ["The Defender's Shield", "Nightmare Shield", "God's Word", "The Greiver's Shield"]
-Rare_Dagger = ["Wooden Dagger", "Aluminum Dagger", "Rusty Metal Dagger", "Kunai"]
-SupRare_Dagger = ["Flame Dagger", "Shock Dagger", "Iron Dagger", "Silver Dagger"]
-Mythic_Dagger = ["God's Reprimand", "Nightmare Dagger", "Roaring Dagger", "DragonScale Dagger"]
-Rare_Dart = ["Wood Dart", "Bronze-tipped Dart", "Splinter-Shot Dart", "Rusted Dart"]
-SupRare_Dart = ["Flaming Dart", "Frozen Dart", "Shocking Dart", "Poison Dart"]
-Mythic_Dart = ["Plagued Dart", "God's Blight", "Nightmare Dart", "Goblin Dart"]
-Rare_Chakram = ["Wood Chakram", "Bronze Chakram", "Light Chakram", "Heavy Chakram"]
-SupRare_Chakram = ["Flame Chakram", "Freeze Chakram", "Dual Chakrams", "Shockram"]
-Mythic_Chakram = ["God's will", "Nightmare Chakram", "Hellfire Chakram", "15000 volt Shockram"]
-Rarity = ["Rare"]*60 + ["Super Rare"]*30 + ["Mythic"]*10
-Pri_roll = random.choice(Loot_pri)
-Sec_roll = random.choice(Loot_sec)
-Rarity_roll = random.choice(Rarity)
-Loot_roll = random.choice(Loot_type)
-Curr_roll = random.choice(Loot_curr)
-Arm_roll = random.choice(Loot_armor)
-Num_roll = random.choice(Curr_amt)
-Start = input("Welcome. Are you prepared to enter? ").lower()
+loot_type = ["Primary Weapon", "Secondary Weapon", "Currency", "Armor"]
+loot_pri = ["Sword", "Bow", "Axe", "Sythe"]
+loot_sec = ["Shield", "Dagger", "Dart", "Chakram"]
+loot_curr = ["Coins", "Gems", "Dark Dollars", "Tokens"]
+curr_amt = ["1", "2", "3", "4", "5"]
+loot_armor = ["Helm", "Chestpiece", "Greaves", "Boots"]
+rare_Sword = ["Bronze Greatsword", "Bronze Shortsword", "Bronze Longsword", "Bronze Rapier"]
+rare_Bow = ["Longbow", "Shortbow", "Strongbow", "Quickbow"]
+rare_Axe = ["Dual Axe", "Great axe", "Handaxe", "Double Handaxe"]
+rare_Sythe = ["Poison sythe", "Quicksythe", "Dual sythe", "Syckle"]
+supRare_Sword = ["Iron Greatsword", "Iron Shortsword", "Iron Longsword", "Iron Rapier"]
+supRare_Bow = ["Flarebow", "Shockbow", "Barbed Bow", "Freezebow"]
+supRare_Axe = ["Waraxe", "Shockaxe", "Flareaxe", "Freezeaxe"]
+supRare_Sythe = ["Poison Syckle", "Cursed sythe", "Circus Blade", "Dual Poison Sythe"]
+mythic_Sword = ["Nightmare Blade", "Roaring Shortsword", "Dragonscale Blade", "God's Wisdom"]
+mythic_Bow = ["Nightmare Bow", "God's Sight", "Fay Bow", "Dwarven Craft"]
+mythic_Axe = ["Nightmare Axe", "God's Rage", "Rude Buster", "Dwarven Hammer"]
+mythic_Sythe = ["Nightmare Sythe", "God's Plague", "Jevilsknife", "Reaper"]
+rare_helm = ["Leather Helm", "Bronze Helm", "Quick Helm", "Strong Helm"]
+supRare_helm = ["Iron Helm", "Flame Helm", "Spider Helm", "Shock Helm"]
+mythic_helm = ["Nightmare Helm", "Hellfire Helm", "God's Touch", "Jevilstail"]
+rare_Chestpiece = ["Leather Chestpiece", "Bronze Chestpiece", "Plate Mail", "Aluminum Chestpiece"]
+supRare_Chestpiece = ["Iron Chestpiece", "Silver Chestpiece", "Rusted Knight Chestpiece", "Tin Chestpiece"]
+mythic_Chestpiece = ["Fallen Knight's Chestpiece", "Nightmare Chestpiece", "God's Heart", "The Hero's Chestpiece"]
+rare_Greaves = ["Wooden Greaves", "Leather Greaves", "Leaf Greaves", "Clay Greaves"]
+supRare_Greaves = ["Iron Greaves", "Silver Greaves", "Rusted Knight Greaves", "Tin Greaves"]
+mythic_Greaves = ["Fallen Knight's Greaves", "Nightmare Greaves", "God's Speed", "The Monarch's Greaves"]
+rare_Boots = ["Leather Boots", "Wooden Boots", "Foil Boots", "Light Weight Boots"]
+supRare_Boots = ["Hermes Boots", "Steel Boots", "Iron Boots", "Heavy Weight Boots"]
+mythic_Boots = ["TerraSpark Boots", "Fallen Knight's Boots", "Nightmare Boots", "The Mourner's Boots", "God's Remorse"]
+rare_Shield = ["Wooden Shield", "Aluminum Shield", "Rusted Metal Shield", "Cardboard Shield"]
+supRare_Shield = ["Iron Shield", "Silver Shield", "Freezing Shield", "Flame Shield"]
+mythic_Shield = ["The Defender's Shield", "Nightmare Shield", "God's Word", "The Greiver's Shield"]
+rare_Dagger = ["Wooden Dagger", "Aluminum Dagger", "Rusty Metal Dagger", "Kunai"]
+supRare_Dagger = ["Flame Dagger", "Shock Dagger", "Iron Dagger", "Silver Dagger"]
+mythic_Dagger = ["God's Reprimand", "Nightmare Dagger", "Roaring Dagger", "DragonScale Dagger"]
+rare_Dart = ["Wood Dart", "Bronze-tipped Dart", "Splinter-Shot Dart", "Rusted Dart"]
+supRare_Dart = ["Flaming Dart", "Frozen Dart", "Shocking Dart", "Poison Dart"]
+mythic_Dart = ["Plagued Dart", "God's Blight", "Nightmare Dart", "Goblin Dart"]
+rare_Chakram = ["Wood Chakram", "Bronze Chakram", "Light Chakram", "Heavy Chakram"]
+supRare_Chakram = ["Flame Chakram", "Freeze Chakram", "Dual Chakrams", "Shockram"]
+mythic_Chakram = ["God's will", "Nightmare Chakram", "Hellfire Chakram", "15000 volt Shockram"]
+rarity = ["Rare"]*60 + ["Super Rare"]*30 + ["Mythic"]*10
+pri_roll = random.choice(loot_pri)
+sec_roll = random.choice(loot_sec)
+rarity_roll = random.choice(rarity)
+loot_roll = random.choice(loot_type)
+curr_roll = random.choice(loot_curr)
+arm_roll = random.choice(loot_armor)
+num_roll = random.choice(curr_amt)
+start = input("Welcome. Are you prepared to enter? ").lower()
 
-if Start == "y" or Start == "yes":
+#def game(player name):
+
+#get player name input 
+#run= game("Jojo")
+
+if start == "y" or start == "yes":
      print("You go into the Dungeon...\n")
      time.sleep(1)
      print("\nDungeon Loot RNG\nIt's time to roll")
-Roll = input("Would you like to roll? ").lower()
-while Roll in ["y", "yes"]:
-    Loot_roll = random.choice(Loot_type)
-    Pri_roll = random.choice(Loot_pri)
-    Rarity_roll = random.choice(Rarity)
-    Sec_roll = random.choice(Loot_sec)
-    Curr_roll = random.choice(Loot_curr)
-    Num_roll = random.choice(Curr_amt)
-    Arm_roll = random.choice(Loot_armor)
-    if Loot_roll == "Primary Weapon":
+else:
+   print("you suck")
+roll = input("Would you like to roll? ").lower()
+while roll in ["y", "yes"]:
+    loot_roll = random.choice(loot_type)
+    pri_roll = random.choice(loot_pri)
+    rarity_roll = random.choice(rarity)
+    sec_roll = random.choice(loot_sec)
+    curr_roll = random.choice(loot_curr)
+    num_roll = random.choice(curr_amt)
+    arm_roll = random.choice(loot_armor)
+    if loot_roll == "Primary Weapon":
      
-     if Rarity_roll == "Rare":
-        if Pri_roll == "Sword":
-            item = random.choice(Rare_Sword)
-        elif Pri_roll == "Bow":
-            item = random.choice(Rare_Bow)
-        elif Pri_roll == "Axe":
-            item = random.choice(Rare_Axe)
-        elif Pri_roll == "Sythe":
-            item = random.choice(Rare_Sythe)
+     if rarity_roll == "Rare":
+        if pri_roll == "Sword":
+            item = random.choice(rare_Sword)
+        elif pri_roll == "Bow":
+            item = random.choice(rare_Bow)
+        elif pri_roll == "Axe":
+            item = random.choice(rare_Axe)
+        elif pri_roll == "Sythe":
+            item = random.choice(rare_Sythe)
 
-     elif Rarity_roll == "Super Rare":
-        if Pri_roll == "Sword":
-            item = random.choice(SupRare_Sword)
-        elif Pri_roll == "Bow":
-            item = random.choice(SupRare_Bow)
-        elif Pri_roll == "Axe":
-            item = random.choice(SupRare_Axe)
-        elif Pri_roll == "Sythe":
-            item = random.choice(SupRare_Sythe)
+     elif rarity_roll == "Super Rare":
+        if pri_roll == "Sword":
+            item = random.choice(supRare_Sword)
+        elif pri_roll == "Bow":
+            item = random.choice(supRare_Bow)
+        elif pri_roll == "Axe":
+            item = random.choice(supRare_Axe)
+        elif pri_roll == "Sythe":
+            item = random.choice(supRare_Sythe)
 
-     elif Rarity_roll == "Mythic":
-        if Pri_roll == "Sword":
-            item = random.choice(Mythic_Sword)
-        elif Pri_roll == "Bow":
-            item = random.choice(Mythic_Bow)
-        elif Pri_roll == "Axe":
-            item = random.choice(Mythic_Axe)
-        elif Pri_roll == "Sythe":
-            item = random.choice(Mythic_Sythe)
+     elif rarity_roll == "pythic":
+        if pri_roll == "Sword":
+            item = random.choice(mythic_Sword)
+        elif pri_roll == "Bow":
+            item = random.choice(mythic_Bow)
+        elif pri_roll == "Axe":
+            item = random.choice(mythic_Axe)
+        elif pri_roll == "Sythe":
+            item = random.choice(mythic_Sythe)
 
-     print(f"You got a {Rarity_roll} {item}")
-    elif Loot_roll == "Secondary Weapon":
-         if Rarity_roll == "Rare":
-            if Sec_roll == "Shield":
-                item = random.choice(Rare_Shield)
-            elif Sec_roll == "Dagger":
-               item = random.choice(Rare_Dagger)
-            elif Sec_roll == "Dart":
-             item = random.choice(Rare_Dart)
-            elif Sec_roll == "Chakram":
-              item = random.choice(Rare_Chakram)
+     print(f"You got a {rarity_roll} {item}")
+    elif loot_roll == "Secondary Weapon":
+         if rarity_roll == "Rare":
+            if sec_roll == "Shield":
+                item = random.choice(rare_Shield)
+            elif sec_roll == "Dagger":
+               item = random.choice(rare_Dagger)
+            elif sec_roll == "Dart":
+             item = random.choice(rare_Dart)
+            elif sec_roll == "Chakram":
+              item = random.choice(rare_Chakram)
 
-         elif Rarity_roll == "Super Rare":
-            if Sec_roll == "Shield":
-             item = random.choice(SupRare_Shield)
-            elif Sec_roll == "Dagger":
-             item = random.choice(SupRare_Dagger)
-            elif Sec_roll == "Dart":
-             item = random.choice(SupRare_Dart)
-            elif Sec_roll == "Chakram":
-             item = random.choice(SupRare_Chakram)
+         elif rarity_roll == "Super Rare":
+            if sec_roll == "Shield":
+             item = random.choice(supRare_Shield)
+            elif sec_roll == "Dagger":
+             item = random.choice(supRare_Dagger)
+            elif sec_roll == "Dart":
+             item = random.choice(supRare_Dart)
+            elif sec_roll == "Chakram":
+             item = random.choice(supRare_Chakram)
 
-         elif Rarity_roll == "Mythic":
-             if Sec_roll == "Shield":
-              item = random.choice(Mythic_Shield)
-             elif Sec_roll == "Dagger":
-                 item = random.choice(Mythic_Dagger)
-             elif Sec_roll == "Dart":
-                  item = random.choice(Mythic_Dart)
-             elif Sec_roll == "Chakram":
-                 item = random.choice(Mythic_Chakram)
-         print(f"You got a {Rarity_roll} {item}")
-    elif Loot_roll == "Currency":
-     print(f"You got {Num_roll} {Curr_roll}")
-    elif Loot_roll == "Armor":
-        if Rarity_roll == "Rare":
-            if Arm_roll == "Helm":
-                item = random.choice(Rare_helm)
-            elif Arm_roll == "Chestpiece":
-             item = random.choice(Rare_Chestpiece)
-            elif Arm_roll == "Greaves":
-             item = random.choice(Rare_Greaves)
-            elif Arm_roll == "Boots":
-             item = random.choice(Rare_Boots)
+         elif rarity_roll == "Mythic":
+             if sec_roll == "Shield":
+              item = random.choice(mythic_Shield)
+             elif sec_roll == "Dagger":
+                 item = random.choice(mythic_Dagger)
+             elif sec_roll == "Dart":
+                  item = random.choice(mythic_Dart)
+             elif sec_roll == "Chakram":
+                 item = random.choice(mythic_Chakram)
+         print(f"You got a {rarity_roll} {item}")
+    elif loot_roll == "Currency":
+     print(f"You got {num_roll} {curr_roll}")
+    elif loot_roll == "Armor":
+        if rarity_roll == "Rare":
+            if arm_roll == "Helm":
+                item = random.choice(rare_helm)
+            elif arm_roll == "Chestpiece":
+             item = random.choice(rare_Chestpiece)
+            elif arm_roll == "Greaves":
+             item = random.choice(rare_Greaves)
+            elif arm_roll == "Boots":
+             item = random.choice(rare_Boots)
 
-        elif Rarity_roll == "Super Rare":
-            if Arm_roll == "Helm":
-                item = random.choice(SupRare_helm)
-            elif Arm_roll == "Chestpiece":
-                item = random.choice(SupRare_Chestpiece)
-            elif Arm_roll == "Greaves":
-                item = random.choice(SupRare_Greaves)
-            elif Arm_roll == "Boots":
-                item = random.choice(SupRare_Boots)
+        elif rarity_roll == "Super Rare":
+            if arm_roll == "Helm":
+                item = random.choice(supRare_helm)
+            elif arm_roll == "Chestpiece":
+                item = random.choice(supRare_Chestpiece)
+            elif arm_roll == "Greaves":
+                item = random.choice(supRare_Greaves)
+            elif arm_roll == "Boots":
+                item = random.choice(supRare_Boots)
 
-        elif Rarity_roll == "Mythic":
-            if Arm_roll == "Helm":
-             item = random.choice(Mythic_helm)
-            elif Arm_roll == "Chestpiece":
-             item = random.choice(Mythic_Chestpiece)
-            elif Arm_roll == "Greaves":
-             item = random.choice(Mythic_Greaves)
-            elif Arm_roll == "Boots":
-             item = random.choice(Mythic_Boots)
-        print(f"You got a {Rarity_roll} {item}")
+        elif rarity_roll == "Mythic":
+            if arm_roll == "Helm":
+             item = random.choice(mythic_helm)
+            elif arm_roll == "Chestpiece":
+             item = random.choice(mythic_Chestpiece)
+            elif arm_roll == "Greaves":
+             item = random.choice(mythic_Greaves)
+            elif arm_roll == "Boots":
+             item = random.choice(mythic_Boots)
+        print(f"You got a {rarity_roll} {item}")
     Roll = input("\nRoll again? ").lower()
+
